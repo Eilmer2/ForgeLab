2c2a75faec4bf9e74ed5faa3b4791893a8f15020
Eilmer2 <151964285+Eilmer2@users.noreply.github.com>
2023-11-29 17:36:07 -0500

    Update copy-script.js

--- copy-script.js
+++ copy-script.js
@@ -1,19 +1,18 @@
-// copy-script.js
-const fs = require('fs-extra');
+const fse = require('fs-extra');
 const path = require('path');
 
-const sourceDir = process.env.CODEBUILD_SRC_DIR;
-const destinationDir = path.join(sourceDir, 'destination');
+const sourceDir = '/codebuild/output/src1218721962/src/github.com/Eilmer2/ForgeLab';
+const destinationDir = '/codebuild/output/src1218721962/src/github.com/Eilmer2/ForgeLabDestination';
+
+console.log('Directorio de origen:', sourceDir);
+console.log('Directorio de destino:', destinationDir);
 
 async function copyFiles() {
   try {
-    await fs.copy(sourceDir, destinationDir, {
-      overwrite: true,
-      errorOnExist: false,
-    });
-    console.log('Files copied successfully!');
-  } catch (error) {
-    console.error('Error copying files:', error);
+    await fse.copy(sourceDir, destinationDir);
+    console.log('¡Archivos copiados con éxito!');
+  } catch (err) {
+    console.error('Error copying files:', err);
     process.exit(1);
   }
 }
