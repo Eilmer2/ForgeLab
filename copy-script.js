120dc998e02f16728a338ca010ac1be6be3aea87
Eilmer2 <151964285+Eilmer2@users.noreply.github.com>
2023-11-29 17:31:57 -0500

    Update copy-script.js

--- copy-script.js
+++ copy-script.js
@@ -1,32 +1,21 @@
 // copy-script.js
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 
-// copy-script.js
 const sourceDir = process.env.CODEBUILD_SRC_DIR;
 const destinationDir = path.join(sourceDir, 'destination');
-const eslintrcPath = path.join(sourceDir, '.eslintrc.js');
 
-
-// Implementa la lógica de copia o cualquier operación que necesites
-// Aquí, simplemente copiamos el contenido de una carpeta a otra
-const copyFiles = () => {
-    console.log(`Directorio de origen: ${sourceDir}`);
-    console.log(`Directorio de destino: ${destinationDir}`);
-
-    fs.readdirSync(sourceDir).forEach(file => {
-        const sourcePath = path.join(sourceDir, file);
-        const destPath = path.join(destinationDir, file);
-
-        // Verificar si el archivo existe antes de copiar
-        if (fs.existsSync(sourcePath)) {
-            fs.copyFileSync(sourcePath, destPath);
-            console.log(`Copiado: ${file}`);
-        } else {
-            console.log(`¡Advertencia! El archivo no existe: ${file}`);
-        }
+async function copyFiles() {
+  try {
+    await fs.copy(sourceDir, destinationDir, {
+      overwrite: true,
+      errorOnExist: false,
     });
-};
+    console.log('Files copied successfully!');
+  } catch (error) {
+    console.error('Error copying files:', error);
+    process.exit(1);
+  }
+}
 
 copyFiles();
-console.log('Copia completada.');
